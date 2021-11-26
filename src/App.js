b07070ae46dbaafac0efc9a35acd13f70717eded
Kristina Alekyan <kristinaalekyan1@gmail.com>
2021-11-26 14:04:55 +0400

    basket

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from "../src/components/Login/Login.js";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WishListContainer from "./components/WishListContainer/WishListContainer";
+import BasketContainer from "./components/BasketContainer/BasktetConainer"
 
 
 class App extends  React.Component {
@@ -25,6 +26,7 @@ class App extends  React.Component {
           <Route exact path="/home" element={<Main/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/wish-list" element={<WishListContainer/>}/>
+          <Route path="/basket" element={<BasketContainer/>}/>
         </Routes>
         
         <Footer/>
